d3a7893021a2032b52b15b0437f15322d3ba4c48
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-11-10 20:37:17 +0300

    Update ServicesConfig.java

--- BookIssurance/src/main/java/ru/hpclab/bd/module1/configuration/ServicesConfig.java
+++ BookIssurance/src/main/java/ru/hpclab/bd/module1/configuration/ServicesConfig.java
@@ -93,7 +93,7 @@ public class ServicesConfig {
         return bookService;
 
     }
-
+ 
     /**
      * Builds and inits new issurance service.
      * @param issuranceRepository issurance repository
