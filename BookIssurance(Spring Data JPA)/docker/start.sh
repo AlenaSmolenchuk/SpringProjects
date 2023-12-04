09fa3363ef9fa6894f3c902e9326e872bb70caed
Alena <124808324+AlenaSmolenchuk@users.noreply.github.com>
2023-12-04 12:46:18 +0300

    Update start.sh

--- BookIssurance(Spring Data JPA)/docker/start.sh
+++ BookIssurance(Spring Data JPA)/docker/start.sh
@@ -1,1 +1,1 @@
-docker run --rm --name pg-docker -e POSTGRES_PASSWORD=minis123 -e POSTGRES_USER=postgres -e POSTGRES_DB=module1 -p 5432 postgres:15
+docker run --rm --name pg-docker -e POSTGRES_PASSWORD=password -e POSTGRES_USER=user -e POSTGRES_DB=data_base -p 5432 postgres:15
